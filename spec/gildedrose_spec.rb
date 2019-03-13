c7b8c9e7e057883db05b218cb54d0dc1e718b9c0
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-03-13 23:07:53 +0000

    testing normal quality depreciation

--- spec/gildedrose_spec.rb
+++ spec/gildedrose_spec.rb
@@ -3,11 +3,20 @@ require 'gildedrose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    context 'quality' do 
+        it "does not change the name" do
+            items = [Item.new("foo", 0, 0)]
+            GildedRose.new(items).update_quality()
+            expect(items[0].name).to eq "foo"
+        end
+        it "quality depreciates by 1 every EOD" do 
+            items = [Item.new("foo", 2, 2)]
+            GildedRose.new(items).update_quality()
+            expect(items[0].quality).to eq 1
+        end
+    
     end
+
   end
 
 end
