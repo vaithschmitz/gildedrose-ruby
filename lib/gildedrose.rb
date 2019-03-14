298579f14cf88bd224f6ffc664fd3b93e4fb7d4a
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-03-14 16:53:02 +0000

    added normal post sellby depreciation

--- lib/gildedrose.rb
+++ lib/gildedrose.rb
@@ -12,48 +12,36 @@ class GildedRose
   
     def update_quality()
       @items.each do |item|
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-            if item.name != "Sulfuras, Hand of Ragnaros"
-              item.quality = item.quality - 1
-            end
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert"
-              if item.sell_in < 11
-                if item.quality < 50
-                  item.quality = item.quality + 1
-                end
-              end
-              if item.sell_in < 6
-                if item.quality < 50
-                  item.quality = item.quality + 1
-                end
-              end
-            end
-          end
-        end
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.sell_in = item.sell_in - 1
-        end
-        if item.sell_in < 0
-          if item.name != "Aged Brie"
-            if item.name != "Backstage passes to a TAFKAL80ETC concert"
-              if item.quality > 0
-                if item.name != "Sulfuras, Hand of Ragnaros"
-                  item.quality = item.quality - 1
-                end
-              end
-            else
-              item.quality = item.quality - item.quality
-            end
-          else
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
+        # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        #   if item.quality > 0
+        #     if item.name != "Sulfuras, Hand of Ragnaros"
+        #       item.quality = item.quality - 1
+        #     end
+        #   end
+        # else
+        #   if item.quality < 50
+        #     item.quality = item.quality + 1
+        #     if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        #       if item.sell_in < 11
+        #         if item.quality < 50
+        #           item.quality = item.quality + 1
+        #         end
+        #       end
+        #       if item.sell_in < 6
+        #         if item.quality < 50
+        #           item.quality = item.quality + 1
+        #         end
+        #       end
+        #     end
+        #   end
+        # end
+        # if item.name != "Sulfuras, Hand of Ragnaros"
+        #   item.sell_in = item.sell_in - 1
+        # end
+        if (item.sell_in <= 0 && (item.name != "Aged Brie" || item.name != "Backstage passes to a TAFKAL80ETC concert" || item.name != "Sulfuras, Hand of Ragnaros") && item.quality > 0)
+            item.quality -= 2
+        elsif (item.sell_in > 0 && (item.name != "Aged Brie" || "Backstage passes to a TAFKAL80ETC concert" || "Sulfuras, Hand of Ragnaros") && item.quality > 0)
+            item.quality -= 1 
         end
       end
     end
--- spec/gildedrose_spec.rb
+++ spec/gildedrose_spec.rb
@@ -2,26 +2,36 @@ require 'gildedrose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    context 'quality' do 
+  describe "quality" do
+    context 'initialized' do 
         it "does not change the name" do
             items = [Item.new("foo", 0, 0)]
             GildedRose.new(items).update_quality()
             expect(items[0].name).to eq "foo"
         end
-        it "quality depreciates by 1 every EOD" do 
-            items = [Item.new("foo", 2, 2)]
-            GildedRose.new(items).update_quality()
-            expect(items[0].quality).to eq 1
-        end
+
         it "quality can never exceed 50" do 
             items = [Item.new("foo", 2, 51)]
             expect{GildedRose.new(items)}.to raise_error("Quality can't exceed 50.")
         end 
+
         it "GR won't accept items with negative quality" do 
             items = [Item.new("foo", 2, -4)]
             expect{GildedRose.new(items)}.to raise_error("Quality can't be negative.")
         end 
+    end
+        
+    context 'depreciation' do
+        it "(pre sell-in) quality depreciates by 1 every EOD" do 
+            items = [Item.new("foo", 2, 2)]
+            GildedRose.new(items).update_quality()
+            expect(items[0].quality).to eq 1
+        end
+        it "(post sell-in) quality depreciates by 2 every EOD" do
+            items = [Item.new("foo", 0, 20)]
+            GildedRose.new(items).update_quality()
+            expect(items[0].quality).to eq 18
+        end
     end
 
   end
